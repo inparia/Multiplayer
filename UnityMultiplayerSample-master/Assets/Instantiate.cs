11aa53646f121032ad8687cceba32ec8a445546f
inparia <inparia@live.ca>
2020-10-13 20:02:24 -0400

    Initial commit

---
+++ UnityMultiplayerSample-master/Assets/Instantiate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+
+    private GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(0, 0.0f, 0);
+        cube.AddComponent<movement>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
